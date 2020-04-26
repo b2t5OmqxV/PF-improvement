29482124c0c2359af83cc750cc902c08efd6c486
b2t5OmqxV <tatuyaizumi7@gmail.com>
2020-04-26 08:45:42 +0000

    [fix]本番環境でのバッチ処理
    記述漏れを追記

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,17 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "Portfolio_#{Rails.env}"
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port: 587,
+    address: 'smtp.gmail.com',
+    domain: 'gmail.com',
+    user_name: ENV['EMAIL_ADDRESS'],
+    password: ENV['EMAIL_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   config.action_mailer.perform_caching = false
 
